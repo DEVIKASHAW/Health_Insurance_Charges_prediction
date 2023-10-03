8ac0112bf6b59126bb47992310813e37bee4cc81
DEVIKASHAW <82443617+DEVIKASHAW@users.noreply.github.com>
2023-10-04 02:08:28 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,28 +1,19 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import sklearn
-from sklearn.ensemble import GradientBoostingClassifier
 
 # Load the pre-trained Gradient Boosting model
-model = joblib.load("model_joblib_gb1.pkl")
+model = joblib.load("model_joblib_gb.pkl")
 
 # Function to predict insurance charges
-def predict_charges(age, sex, bmi, children, smoker, region):
+def predict_charges(age, sex, bmi, children, smoker, region_northeast, region_northwest, region_southeast, region_southwest):
     # Mapping categorical variables to numerical values
     sex_mapping = {"Male": 1, "Female": 0}
     smoker_mapping = {"Yes": 1, "No": 0}
-    region_mapping = {
-        "Northeast": 0,
-        "Northwest": 1,
-        "Southeast": 2,
-        "Southwest": 3
-    }
 
     # Encoding categorical variables
     sex_encoded = sex_mapping.get(sex, 0)
     smoker_encoded = smoker_mapping.get(smoker, 0)
-    region_encoded = region_mapping.get(region, 0)
     
     # Create a DataFrame for prediction
     input_data = pd.DataFrame({
@@ -31,10 +22,10 @@ def predict_charges(age, sex, bmi, children, smoker, region):
         'bmi': [bmi],
         'children': [children],
         'smoker': [smoker_encoded],
-        'region_northeast': [1 if region_encoded == 0 else 0],
-        'region_northwest': [1 if region_encoded == 1 else 0],
-        'region_southeast': [1 if region_encoded == 2 else 0],
-        'region_southwest': [1 if region_encoded == 3 else 0]
+        'region_northeast': [region_northeast],
+        'region_northwest': [region_northwest],
+        'region_southeast': [region_southeast],
+        'region_southwest': [region_southwest]
     })
     
     # Make prediction
@@ -51,9 +42,12 @@ sex = st.sidebar.radio("Sex", ["Male", "Female"])
 bmi = st.sidebar.slider("BMI", 15.0, 50.0, 25.0)
 children = st.sidebar.slider("Number of Children", 0, 5, 1)
 smoker = st.sidebar.radio("Smoker", ["Yes", "No"])
-region = st.sidebar.radio("Region", ["Northeast", "Northwest", "Southeast", "Southwest"])
+region_northeast = st.sidebar.checkbox("Northeast")
+region_northwest = st.sidebar.checkbox("Northwest")
+region_southeast = st.sidebar.checkbox("Southeast")
+region_southwest = st.sidebar.checkbox("Southwest")
 
 # Make prediction when the "Predict" button is clicked
 if st.sidebar.button("Predict"):
-    prediction = predict_charges(age, sex, bmi, children, smoker, region)
+    prediction = predict_charges(age, sex, bmi, children, smoker, region_northeast, region_northwest, region_southeast, region_southwest)
     st.subheader(f"Predicted Insurance Charges: ${prediction:.2f}")
